4721ade52414ba9a3af4c421b03c90418fd90d5a
Tess <tess.merrill25@gmail.com>
2023-07-17 21:40:21 -0600

    complete arrow functions lab

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    return 2000/100
+}
+
+const square = (num) => num * num;
+
+const add = (num1, num2) => num1 + num2;
+
+// 3) has an arrow function called square
+// 4) square arrow function takes one parameter and multiplies it times itself
+// 5) has an arrow function called add
+// 6) add arrow function takes two parameters and adds them together
